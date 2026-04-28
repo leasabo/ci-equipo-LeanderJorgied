ddb2c989bc5084a3bc61ae3423db56917f3b79b5
Jorge Lencina <jorge.lencina@cablasociados.com>
2026-04-28 00:43:38 -0300

    Agregar función palíndromo

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from src.utils import saludar, invertir_texto, contar_palabras
+from src.utils import saludar, invertir_texto, contar_palabras, es_palindromo
 
 def main():
     print(saludar("Carlos"))
     print(saludar("Jorge"))
     print(invertir_texto("hola mundo"))
     print(contar_palabras("Anita lava la tina"))
+    print(es_palindromo("Anita lava la tina"))
 
 if __name__ == "__main__":
     main()
